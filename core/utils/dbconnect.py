979de6b16d8115a8e1add3dfb55c42f8107f278f
DiLDDoS <ctulhuthemonster@gmail.com>
2022-12-06 19:16:21 +0300

    i18n

--- core/utils/dbconnect.py
+++ core/utils/dbconnect.py
@@ -6,9 +6,9 @@ class Request:
         self.connector = connector
 
     async def new_user(self, user_id, username):
-        query = """INSERT INTO UserInfo (user_id, username, locale) VALUES ($1, $2, 'en')
+        query = """INSERT INTO UserInfo (user_id, username, locale) VALUES ($1, $2, $3)
                 ON CONFLICT (user_id) DO UPDATE SET username=$2"""
-        await self.connector.execute(query, user_id, username)
+        await self.connector.execute(query, user_id, username, 'en')
 
     async def get_locale(self, user_id):
         query = """SELECT locale FROM UserInfo WHERE user_id = $1"""
