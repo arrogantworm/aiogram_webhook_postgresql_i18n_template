dce3c7ef0ce1bcc56e14623a7f3d05b745227900
DiLDDoS <ctulhuthemonster@gmail.com>
2022-12-06 13:09:47 +0300

    i18n

--- bot.py
+++ bot.py
@@ -46,10 +46,7 @@ async def start():
     dp.shutdown.register(on_shutdown)
 
     # Middlewares
-    dp.message.middleware.register(ConstI18nMiddleware(locale='ru', i18n=i18n), Command(commands=['start']))
-    dp.message.middleware.register(ConstI18nMiddleware(locale='en', i18n=i18n), Command(commands=['en']))
-    # dp.update.middleware.register(ConstI18nMiddleware(locale='ru', i18n=i18n), Command(commands=['start']))
-    # dp.update.middleware.register(ConstI18nMiddleware(locale='en', i18n=i18n), Command(commands=['en']))
+    dp.update.middleware.register(ConstI18nMiddleware(locale='ru', i18n=i18n))
     dp.update.middleware.register(dbmiddleware.DbSession(pool_connect))
 
     # Routers
