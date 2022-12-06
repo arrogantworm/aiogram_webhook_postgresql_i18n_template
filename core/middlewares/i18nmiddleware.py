aa2f221440fd00b2bc3376ed7800602ee386bef9
DiLDDoS <ctulhuthemonster@gmail.com>
2022-12-06 16:51:50 +0300

    i18n

--- core/middlewares/i18nmiddleware.py
+++ core/middlewares/i18nmiddleware.py
@@ -1,18 +1,43 @@
-from aiogram.utils.i18n import I18nMiddleware
+from aiogram.utils.i18n import I18nMiddleware, SimpleI18nMiddleware
 from aiogram.types import TelegramObject, Message
-from typing import Callable, Awaitable, Dict, Any
+from abc import ABC, abstractmethod
+from typing import Any, Awaitable, Callable, Dict, Optional, Set, cast
 from core.utils.dbconnect import Request
+try:
+    from babel import Locale, UnknownLocaleError
+except ImportError:  # pragma: no cover
+    Locale = 'en'
+from aiogram import BaseMiddleware, Router
+from aiogram.fsm.context import FSMContext
+from aiogram.types import TelegramObject, User
+from aiogram.utils.i18n.core import I18n
 
 
-class DBI18nMiddleware(I18nMiddleware):
+class DBI18nMiddleware(SimpleI18nMiddleware):
 
-    async def __call__(self,
-                       handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-                       event: Message,
-                       data: Dict[str, Any]):
-        data['user_id'] = event.from_user.id
-        data['request'] = Request
-        return await handler(event, data)
+    def __init__(
+        self,
+        i18n: I18n,
+        key: str = "locale",
+        i18n_key: Optional[str] = "i18n",
+        middleware_key: str = "i18n_middleware",
+    ) -> None:
+        super().__init__(i18n=i18n, i18n_key=i18n_key, middleware_key=middleware_key)
+        self.key = key
 
     async def get_locale(self, event: TelegramObject, data: Dict[str, Any]) -> str:
-        return await Request.get_locale(self=data['request'], user_id=data['user_id'])
+        event_from_user: Optional[User] = data.get("event_from_user", None)
+        if event_from_user is None or event_from_user.id is None:
+            return self.i18n.default_locale
+        try:
+            locale = Request.get_locale(event_from_user.id)
+            print(locale)
+        except:
+            return self.i18n.default_locale
+        if locale not in self.i18n.available_locales:
+            return self.i18n.default_locale
+        return cast(str, locale)
+
+    async def set_locale(self, state: FSMContext, locale: str) -> None:
+        await state.update_data(data={self.key: locale})
+        self.i18n.current_locale = locale
