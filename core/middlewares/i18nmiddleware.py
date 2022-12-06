a254c65f1dbadbf843ffa324a3434a766a434278
DiLDDoS <ctulhuthemonster@gmail.com>
2022-12-06 16:23:59 +0300

    i18n

--- core/middlewares/i18nmiddleware.py
+++ core/middlewares/i18nmiddleware.py
@@ -7,12 +7,12 @@ from core.utils.dbconnect import Request
 class DBI18nMiddleware(I18nMiddleware):
 
     async def __call__(self,
-                       handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-                       message: Message,
+                       handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
+                       event: Message,
                        data: Dict[str, Any]):
-        data['user_id'] = message.from_user.id
+        data['user_id'] = event.from_user.id
         data['request'] = Request
-        return await handler(message, data)
+        return await handler(event, data)
 
     async def get_locale(self, event: TelegramObject, data: Dict[str, Any]) -> str:
         return await Request.get_locale(self=data['request'], user_id=data['user_id'])
