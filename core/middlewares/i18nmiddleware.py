0161c1a1d50a474f5845c0f60ce534155c483cb2
DiLDDoS <ctulhuthemonster@gmail.com>
2022-12-06 16:18:01 +0300

    i18n

--- core/middlewares/i18nmiddleware.py
+++ core/middlewares/i18nmiddleware.py
@@ -8,9 +8,10 @@ class DBI18nMiddleware(I18nMiddleware):
 
     async def __call__(self,
                        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
-                       event: Message,
+                       message: Message,
                        data: Dict[str, Any]):
-        data['user_id'] = event.from_user.id
+        data['user_id'] = message.from_user.id
+        data['request'] = Request
 
     async def get_locale(self, event: TelegramObject, data: Dict[str, Any]) -> str:
-        return await Request.get_locale(user_id=data['user_id'])
+        return await Request.get_locale(self=data['request'], user_id=data['user_id'])
