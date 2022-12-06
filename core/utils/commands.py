e5a4dee6c03100c34484206e5783696cae5747d9
arrogantworm <58707098+arrogantworm@users.noreply.github.com>
2022-12-06 21:02:38 +0300

    Update commands.py

--- core/utils/commands.py
+++ core/utils/commands.py
@@ -5,28 +5,12 @@ from aiogram.types import BotCommand, BotCommandScopeDefault
 async def set_commands(bot: Bot):
     commands = [
         BotCommand(
-            command='menu',
-            description='Show bot menu'
+            command='start',
+            description='Bot start'
         ),
         BotCommand(
             command='cancel',
             description='Cancel current action'
-        ),
-        BotCommand(
-            command='catalogue',
-            description='Show catalogue'
-        ),
-        BotCommand(
-            command='payment',
-            description='Payment'
-        ),
-        BotCommand(
-            command='profile',
-            description='My profile'
-        ),
-        BotCommand(
-            command='question',
-            description='Support ticket'
         ),
         BotCommand(
             command='lang',
