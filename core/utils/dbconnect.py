5d6152b3c503562375dcddb298b471ac020bb96f
DiLDDoS <ctulhuthemonster@gmail.com>
2022-12-06 16:11:33 +0300

    i18n

--- core/utils/dbconnect.py
+++ core/utils/dbconnect.py
@@ -12,9 +12,9 @@ class Request:
 
     async def get_locale(self, user_id):
         query = """SELECT locale FROM UserInfo WHERE user_id = $1"""
-        locale = await self.connector.fetch(query, (user_id,))
-        print(locale)
-        return locale
+        locale = await self.connector.fetchrow(query, (user_id,))
+        print(locale['locale'])
+        return locale['locale']
 
     async def set_locale(self, user_id, locale):
         query = """UPDATE UserInfo SET VALUES locale=$2 WHERE user_id = $1"""
