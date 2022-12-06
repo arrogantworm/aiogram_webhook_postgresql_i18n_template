58cd3b83c8b2584a8500fc0458f1a91ff45dd9c4
DiLDDoS <ctulhuthemonster@gmail.com>
2022-12-06 12:41:09 +0300

    i18n

--- core/utils/commands.py
+++ core/utils/commands.py
@@ -1,37 +1,36 @@
 from aiogram import Bot
 from aiogram.types import BotCommand, BotCommandScopeDefault
-from aiogram.utils.i18n import lazy_gettext as __
 
 
 async def set_commands(bot: Bot):
     commands = [
         BotCommand(
             command='start',
-            description=__('Начало работы')
+            description='Начало работы'
         ),
         BotCommand(
             command='cancel',
-            description=__('Отменить действие')
+            description='Отменить действие'
         ),
         BotCommand(
             command='catalogue',
-            description=__('Каталог')
+            description='Каталог'
         ),
         BotCommand(
             command='payment',
-            description=__('Пополнить баланс')
+            description='Пополнить баланс'
         ),
         BotCommand(
             command='profile',
-            description=__('Мой профиль')
+            description='Мой профиль'
         ),
         BotCommand(
             command='question',
-            description=__('Задать вопрос')
+            description='Задать вопрос'
         ),
         BotCommand(
             command='lang',
-            description=__('Изменить язык')
+            description='Изменить язык'
         ),
     ]
 
